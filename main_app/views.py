cc06aaf3b560252629b36f76c69fd5101f96624d
aya ehab <ayaaehab1999@gmail.com>
2024-12-01 14:42:16 +0300

    using-djangos-orm-in-the-app

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
+from .models import Cat
 
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# # Create a list of Cat instances
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -23,4 +24,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()  
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
