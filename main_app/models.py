d7153a8834d2d0251eecefe11ee3566e4d1eff88
aya ehab <ayaaehab1999@gmail.com>
2024-12-01 18:44:26 +0300

    class Feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -14,3 +19,21 @@ class Cat(models.Model):
     def get_absolute_url(self):
     # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
         return reverse('cat-detail', kwargs={'cat_id': self.id})
+    
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+    # Create a cat_id column for each feeding in the database    
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+    
